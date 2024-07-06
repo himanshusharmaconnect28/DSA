1d1f614b81825a1dc41b8431acb8382fb9020a95
himanshusharmaconnect28 <himanshusharmaconnect28@gmail.com>
2024-07-06 21:45:12 +0530

    Update Finding_lower_pivot_index.cpp

--- BINARY_SEARCH/Finding_lower_pivot_index.cpp
+++ BINARY_SEARCH/Finding_lower_pivot_index.cpp
@@ -1,23 +1,24 @@
 #include<iostream>
 using namespace std;
-int Find_Peak_Index(int arr[], int size) {
+int Find_Pivot_Index(int arr[], int size) {
     int start = 0;
     int end = size - 1;
-    while(start < end) {
+    while (start < end) {
         int mid = start + (end - start) / 2;
-        if (arr[mid] < arr[mid + 1]) {
+        if (arr[mid] > arr[mid + 1]) {
             start = mid + 1;
         }
-        else {
+        else{
             end = mid;
         }
     }
     return start;
 }
 int main() {
-    int arr[] = {0, 2, 5, 3, 1};
+    int arr[] = {8, 10, 1, 3, 5};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int peak_index = Find_Peak_Index(arr, size);
-    cout<<"The peak element is : "<<arr[peak_index]<<endl;
+    int pivot_index = Find_Pivot_Index(arr, size);
+    cout << "The pivot element is at index: " << pivot_index << endl;
+    cout << "The pivot element is: " << arr[pivot_index] << endl;
     return 0;
 }
